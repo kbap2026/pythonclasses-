9067c21a3c960fc21c2b32cd2507a60845134c0b
kbap2026 <kbap2026@gmail.com>
2026-07-10 20:31:11 +0530

    Add files via upload

---
+++ slider.py
@@ -0,0 +1,66 @@
+# Python program to demonstrate
+# scale widget
+
+from tkinter import *
+#
+root = Tk()
+root.geometry("400x300")
+#
+# v1 = DoubleVar()
+#
+#
+# def show1():
+#     sel = "Horizontal Scale Value = " + str(v1.get())
+#     l1.config(text=sel, font=("Courier", 14))
+#
+#
+# s1 = Scale(root, variable=v1,
+#            from_=1, to=100,
+#            orient=HORIZONTAL)
+#
+# l3 = Label(root, text="Horizontal Scaler")
+#
+# b1 = Button(root, text="Display Horizontal",
+#             command=show1,
+#             bg="yellow")
+#
+# l1 = Label(root)
+#
+# s1.pack(anchor=CENTER)
+# l3.pack()
+# b1.pack(anchor=CENTER)
+# l1.pack()
+
+root.mainloop()
+
+from tkinter import *
+
+root = Tk()
+root.geometry("400x300")
+v2 = DoubleVar()
+
+
+def show2():
+    sel = "Vertical Scale Value = " + str(v2.get())
+    l2.config(text=sel, font=("Courier", 14))
+
+
+s2 = Scale(root, variable=v2,
+           from_=50, to=1,
+           orient=VERTICAL)
+
+l4 = Label(root, text="Vertical Scaler")
+
+b2 = Button(root, text="Display Vertical",
+            command=show2,
+            bg="purple",
+            fg="white")
+
+l2 = Label(root)
+
+s2.pack(anchor=CENTER)
+l4.pack()
+b2.pack()
+l2.pack()
+
+root.mainloop()
